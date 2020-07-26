72ed2031fa7f42d4332a68c8aa5e5e690f4ef832
devjhin <jhin0904@gmail.com>
2020-07-27 04:37:01 +0900

    ExampleProject 업로드

---
+++ Examples/Chapter2Project/Assets/CubeMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Vector3.forward;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Vector3.right;
+        }
+    }
+}
---
+++ Examples/Chapter2Project/Assets/TestScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Called!");
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("OnEnable Called!");
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("OnDisable Called!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update Called!");       
+        transform.position += Vector3.forward*Time.deltaTime; 
+    }
+
+
+}
